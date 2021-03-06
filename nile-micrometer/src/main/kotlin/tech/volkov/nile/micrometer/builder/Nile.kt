e3b8b1258b2e1920c7c8eebe9c28bb675614f20b
Andrey Volkov <a.volkov23@tinkoff.ru>
2021-03-06 16:48:37 +0300

    update code

--- nile-micrometer/src/main/kotlin/tech/volkov/nile/micrometer/builder/Nile.kt
+++ nile-micrometer/src/main/kotlin/tech/volkov/nile/micrometer/builder/Nile.kt
@@ -20,6 +20,11 @@ class Nile private constructor(
      */
     corePoolSize: Int,
     /**
+     * By default Nile uses task scheduler with single thread to build metrics by scheduling.
+     * In case you do not need scheduled metrics, you can disable scheduling and single thread won't be created.
+     */
+    disableScheduling: Boolean,
+    /**
      * The time interval between each metrics collection, this value is used
      * in case no [MetricParametersContext.scrapeInterval] is specified.
      *
@@ -32,14 +37,20 @@ class Nile private constructor(
     private var taskScheduler: TaskScheduler = TaskScheduler(corePoolSize, defaultScrapeInterval, metricsRegister)
 
     companion object : KLogging() {
+        private const val DEFAULT_DISABLE_SCHEDULING = false
         private const val DEFAULT_CORE_POOL_SIZE = 1
         private val DEFAULT_SCRAPE_INTERVAL = Duration.ofMinutes(1)
 
         fun builder() = NileBuilder()
 
         class NileBuilder {
+            private var disableScheduling = DEFAULT_DISABLE_SCHEDULING
             private var corePoolSize = DEFAULT_CORE_POOL_SIZE
             private var defaultScrapeInterval = DEFAULT_SCRAPE_INTERVAL
+
+            fun disableScheduling() = apply {
+                disableScheduling = true
+            }
 
             fun corePoolSize(poolSize: Int): NileBuilder = apply {
                 corePoolSize = poolSize
@@ -49,10 +60,11 @@ class Nile private constructor(
                 defaultScrapeInterval = scrapeInterval
             }
 
-            fun build() = Nile(corePoolSize, defaultScrapeInterval)
+            fun build() = Nile(corePoolSize, disableScheduling, defaultScrapeInterval)
                 .also { logger.info { "Nile configured with properties: $this" } }
 
             override fun toString() = listOf(
+                "scheduler.enabled: ${!disableScheduling}",
                 "scheduler.core.pool.size: $corePoolSize",
                 "default.scrape.interval: $defaultScrapeInterval"
             )
