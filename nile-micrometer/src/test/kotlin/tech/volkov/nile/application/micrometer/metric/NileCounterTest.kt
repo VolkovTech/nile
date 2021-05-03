276d2b1dbf8668262fe87254ed6d0106c80d4c36
Andrey Volkov <a.volkov23@tinkoff.ru>
2021-05-03 16:37:22 +0300

    update counter

---
+++ nile-micrometer/src/test/kotlin/tech/volkov/nile/application/micrometer/metric/NileCounterTest.kt
@@ -0,0 +1,27 @@
+package tech.volkov.nile.application.micrometer.metric
+
+import io.kotest.assertions.asClue
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import tech.volkov.nile.application.micrometer.registry
+import tech.volkov.nile.micrometer.metric.incrementCounter
+
+internal class NileCounterTest {
+
+    @Test
+    fun `counter correctly increments`() {
+        registry.find(TEST_COUNTER_NAME).counters() shouldHaveSize 0
+
+        incrementCounter(TEST_COUNTER_NAME)
+
+        registry.find(TEST_COUNTER_NAME).counters().asClue {
+            it shouldHaveSize 1
+            it.first().id.name shouldBe TEST_COUNTER_NAME
+        }
+    }
+
+    companion object {
+        private const val TEST_COUNTER_NAME = "test_counter"
+    }
+}
