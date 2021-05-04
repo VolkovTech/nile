3088aa8485df7c0f1bb49bb83c7b587bfa0a34d2
Andrey Volkov <a.volkov23@tinkoff.ru>
2021-05-04 07:35:57 +0300

    add update

--- nile-micrometer/src/main/kotlin/tech/volkov/nile/micrometer/configuration/NileAutoConfiguration.kt
+++ nile-micrometer/src/main/kotlin/tech/volkov/nile/micrometer/configuration/NileAutoConfiguration.kt
@@ -3,10 +3,22 @@ package tech.volkov.nile.micrometer.configuration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import tech.volkov.nile.micrometer.annotation.aspect.NileCounterAspect
+import tech.volkov.nile.micrometer.annotation.aspect.NileDistributionSummaryAspect
+import tech.volkov.nile.micrometer.annotation.aspect.NileGaugeAspect
+import tech.volkov.nile.micrometer.annotation.aspect.NileTimerAspect
 
 @Configuration
 class NileAutoConfiguration {
 
     @Bean
     fun nileCounterAspect() = NileCounterAspect()
+
+    @Bean
+    fun nileTimerAspect() = NileTimerAspect()
+
+    @Bean
+    fun nileGaugeAspect() = NileGaugeAspect()
+
+    @Bean
+    fun nileDistributionSummaryAspect() = NileDistributionSummaryAspect()
 }
