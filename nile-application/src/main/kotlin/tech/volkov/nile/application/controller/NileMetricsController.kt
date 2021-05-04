37cb6fe24d4455fa37bf59d17b58a2cad08723e6
Andrey Volkov <a.volkov23@tinkoff.ru>
2021-05-04 07:06:37 +0300

    add NileGaugeAspect

--- nile-application/src/main/kotlin/tech/volkov/nile/application/controller/NileMetricsController.kt
+++ nile-application/src/main/kotlin/tech/volkov/nile/application/controller/NileMetricsController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 import tech.volkov.nile.application.dto.MetricDto
 import tech.volkov.nile.application.service.DogFactService
 import tech.volkov.nile.micrometer.annotation.metric.NileCounter
+import tech.volkov.nile.micrometer.annotation.metric.NileGauge
 import tech.volkov.nile.micrometer.annotation.metric.NileTimer
 import tech.volkov.nile.micrometer.scheduler.NileScheduler
 import java.time.Duration
@@ -23,14 +24,19 @@ class NileMetricsController(
     @NileCounter(
         name = "dog_facts_counter",
         description = "Counts how many request were executed to dog facts API",
-        tags = ["isAnnotation", "true", "amount", "10"],
+        tags = ["tagName", "tagValue"],
         amount = 10.0
     )
     @NileTimer(
         name = "dog_facts_timer",
         description = "Response time for dog facts API",
-        tags = ["isAnnotation", "true"],
+        tags = ["tagName", "tagValue"],
         percentiles = [0.5, 0.75, 0.9, 0.95, 0.99]
+    )
+    @NileGauge(
+        name = "dog_facts_gauge",
+        description = "Current value of size of dogs facts",
+        tags = ["tagName", "tagValue"]
     )
     fun getDogFacts() = dogFactService.getFacts()
 
---
+++ nile-micrometer/src/main/kotlin/tech/volkov/nile/micrometer/annotation/aspect/NileGaugeAspect.kt
@@ -0,0 +1,32 @@
+package tech.volkov.nile.micrometer.annotation.aspect
+
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import tech.volkov.nile.micrometer.annotation.metric.NileGauge
+import tech.volkov.nile.micrometer.annotation.metric.NileTimer
+import tech.volkov.nile.micrometer.global.getTags
+import tech.volkov.nile.micrometer.metric.nileCounter
+import tech.volkov.nile.micrometer.metric.nileGauge
+import tech.volkov.nile.micrometer.metric.nileTimer
+
+@Aspect
+class NileGaugeAspect {
+
+    @Around("@annotation(tech.volkov.nile.micrometer.annotation.metric.NileGauge)")
+    fun around(joinPoint: ProceedingJoinPoint): Any {
+        val method = joinPoint.target.javaClass.getMethod(joinPoint.signature.name)
+        val gauge = method.getAnnotation(NileGauge::class.java)
+
+        try {
+            val result = joinPoint.proceed()
+            nileGauge(gauge.name, gauge.description, getTags(gauge.tags), true) {
+                (result as? Double) ?: 0.0
+            }
+            return result
+        } catch (ex: Exception) {
+            nileGauge(gauge.name, gauge.description, getTags(gauge.tags), false)
+            throw ex
+        }
+    }
+}
