5cdcf70ce20d4532e345f8ca4633de372eb90f42
Andrey Volkov <a.volkov23@tinkoff.ru>
2021-05-20 21:49:35 +0300

    update all

--- nile-application/src/main/kotlin/tech/volkov/nile/application/controller/NileFunctionsController.kt
+++ nile-application/src/main/kotlin/tech/volkov/nile/application/controller/NileFunctionsController.kt
@@ -8,6 +8,7 @@ import tech.volkov.nile.micrometer.metric.nileCounter
 import tech.volkov.nile.micrometer.metric.nileDistributionSummary
 import tech.volkov.nile.micrometer.metric.nileGauge
 import tech.volkov.nile.micrometer.metric.nileTimer
+import kotlin.random.Random
 
 @RestController
 @RequestMapping("dog-facts/function")
@@ -17,38 +18,39 @@ class NileFunctionsController(
 
     @GetMapping("counter")
     fun counter() = nileCounter(
-        name = "dog_facts_counter",
-        description = "Counts how many request were executed to dog facts API",
-        tags = mapOf("tagName" to "tagValue")
+        name = "requests_counter",
+        description = "Counts how many request were executed to API",
+//        tags = mapOf("tagName" to "tagValue")
     ) {
         dogFactService.getFacts()
     }
 
     @GetMapping("timer")
     fun timer() = nileTimer(
-        name = "dog_facts_timer",
-        description = "Response time for dog facts API",
-        tags = mapOf("tagName" to "tagValue"),
+        name = "response_time",
+        description = "Response time for external API",
+//        tags = mapOf("tagName" to "tagValue"),
         percentiles = listOf(0.5, 0.75, 0.9, 0.95, 0.99)
     ) {
+        Thread.sleep(Random.nextLong(500))
         dogFactService.getFacts()
     }
 
     @GetMapping("gauge")
     fun gauge() = nileGauge(
-        name = "dog_facts_gauge",
-        description = "Current value of size of dogs facts",
-        tags = mapOf("tagName" to "tagValue")
+        name = "requests_gauge",
+        description = "Current value of size of returned list from API",
+//        tags = mapOf("tagName" to "tagValue")
     ) {
         dogFactService.getFacts().size.toDouble()
     }
 
     @GetMapping("distribution-summary")
     fun distributionSummary() = nileDistributionSummary(
-        name = "dog_facts_distribution_summary",
+        name = "requests_distribution_summary",
         unit = "short",
-        description = "Distribution summary for dogs facts",
-        tags = mapOf("tagName" to "tagValue")
+        description = "Distribution summary for API requests",
+//        tags = mapOf("tagName" to "tagValue")
     ) {
         dogFactService.getFacts().size.toDouble()
     }
